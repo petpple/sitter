776c3178af4dc882da937c45c7f8c65508898119
2kkeullim <rosel0314@mail.hongik.ac.kr>
2023-01-06 18:51:29 +0900

    ⚙️ :: 프로젝트 초기 설정 - 보영

---
+++ src/main/java/petpple/kiwi/sitter/SitterApplication.java
@@ -0,0 +1,13 @@
+package petpple.kiwi.sitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitterApplication.class, args);
+	}
+
+}
