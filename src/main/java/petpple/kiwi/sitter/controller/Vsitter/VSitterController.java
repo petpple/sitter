c102c552d7599fa113727438a34757bebb30f829
flown222 <jangu3384@gmail.com>
2023-01-17 16:42:31 +0900

    ⚡️ :: 방문펫시터  컨트롤러수정 -동현
    ⚡️ :: 방문펫시터  컨트롤러수정 -동현

--- src/main/java/petpple/kiwi/sitter/controller/Vsitter/VSitterController.java
+++ src/main/java/petpple/kiwi/sitter/controller/Vsitter/VSitterController.java
@@ -1,11 +1,27 @@
 package petpple.kiwi.sitter.controller.Vsitter;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import petpple.kiwi.sitter.domain.Vsitter;
+import petpple.kiwi.sitter.repository.IVsitterMapper;
+import petpple.kiwi.sitter.repository.sitter.ISitterMapper;
 
 @Controller
 public class VSitterController {
 
+	@Autowired 
+	private SqlSession sqlSession; 
+	
 	@RequestMapping("/vsitter/index")
 	public String vsitterIndex()
 	{
@@ -17,10 +33,35 @@ public class VSitterController {
 	{
 		return "vsitter/sample";
 	}
+	
+	@RequestMapping("sitter/sitterInfoVsvcWaiting")
+	public String sitterInfoVsvcWaiting(Model model, HttpServletRequest request) {
+		
+		IVsitterMapper dao = sqlSession.getMapper(IVsitterMapper.class);
+		HttpSession session = request.getSession();
+		
 
+		String temId = (String) session.getAttribute("temId");
+
+		if (temId == null) {
+			return "//";
+		}
+		
+		ArrayList<Vsitter> list= dao.requestVisitService(temId);
+		model.addAttribute("list", list);
+		return "sitter/sitterInfoVsvcWaiting";
+	}
 //	@RequestMapping("vsitter/vsitterSide")
 //	public String vsitterSide() {
 //		return "vsitter/vsitterSide";
 //	}
 
+	@RequestMapping("sitter/VSvcreservation")
+	public String sitterInfoVsvcWaiting(@RequestParam("vId") String vId) {
+		
+		IVsitterMapper dao = sqlSession.getMapper(IVsitterMapper.class);
+		dao.reserveVisit(vId);
+		
+		return "sitter/sitterMain";
+	}
 }
