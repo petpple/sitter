849f1512b3c5b327a4de18f45b30b017085a2981
SOYEON <tivm8012@naver.com>
2023-01-17 10:14:12 +0900

    ⚡️ ::  dto 속성 수정 - 소연
    ⚡️ ::  dto 속성 수정 - 소연

--- src/main/java/petpple/kiwi/sitter/domain/Vsitter.java
+++ src/main/java/petpple/kiwi/sitter/domain/Vsitter.java
@@ -7,6 +7,6 @@ public class Vsitter
 {
 	// 방문 요청번호, 이름, 주소, 서비스시작일, 종료일, 펫시터 닉네임, 의뢰인 프로필
 	private String id, temId, vid, name, addr, startDate, endDate, clientNickname
-	, clientProfile, reqDate, totalPrice, service;
+	, clientProfile, reqDate, totalPrice;
 
 }
