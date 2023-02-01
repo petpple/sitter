a960015b3a09b413f5cb66cfccbf21536257f312
hyunkkong <gustjd8979@gmail.com>
2023-02-01 20:12:01 +0900

    ⚡️ :: 위탁펫시터 dto 수정 - 현성
    ⚡️ :: 위탁펫시터 dto 수정 - 현성

--- src/main/java/petpple/kiwi/sitter/domain/Fsitter.java
+++ src/main/java/petpple/kiwi/sitter/domain/Fsitter.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class Fsitter
 {
-	private String id;
-
+	// 위탁펫시터 신고번호, 위탁펫시터 거주지 사진 123,가구원 흡연 여부, 거주지 유형, 거주지 규모, 위탁 프로필 공개 일자, 펫시터 승인번호(id)
+	private String fsitId, resImg1, resImg2, resImg3, fsitPubDate, resType, resSize, smoking, allowId ;
 }
